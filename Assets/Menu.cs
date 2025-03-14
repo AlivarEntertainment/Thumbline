031f286a61dd510d209d1fdc4822027b6b5c15e5
AlivarEntertainment <AlivarEnt@yandex.ru>
2025-03-14 23:24:11 +0500

    firstScene

---
+++ Assets/Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class Menu : MonoBehaviour
+{   
+    public PlayableDirector StartCutScene;
+    public void OnStartButtonClick()
+    {
+        StartCutScene.Play();
+        Destroy(this.gameObject);
+    }
+}
